8903f261f9853300829c75ef95398dd9a7eb83cf
Dylan De Sousa <dylan.de-sousa@arsen.co>
2025-09-01 01:38:01 +0200

    fix(auth): missing AuthGuard, applied to all endpoints by default

--- src/identity-and-access/identity-and-access.module.ts
+++ src/identity-and-access/identity-and-access.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard, AuthModule } from '@thallesp/nestjs-better-auth';
 import { auth } from './infrastructure/better-auth.js';
 
 @Module({
   imports: [AuthModule.forRoot(auth)],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class IdentityAndAccessModule {}
