e70cd075ee38eba5bc58a956a098ad7ab1ae75bd
Dylan De Sousa <dylan.de-sousa@arsen.co>
2026-08-20 19:46:33 +0200

    test: combine vitest coverage reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,30 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [tsconfigPaths(), swc.vite(), swc.rollup()],
   test: {
-    exclude: ["build", "node_modules", "src/**/*.integration.spec.ts"],
-    include: ["src/**/*.spec.ts"],
+    coverage: {
+      exclude: ["src/**/*.spec.ts", "src/**/*.integration.spec.ts"],
+      include: ["src/**/*.ts"],
+      provider: "v8",
+    },
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          exclude: ["build", "node_modules", "src/**/*.integration.spec.ts"],
+          include: ["src/**/*.spec.ts"],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: "integration",
+          exclude: ["build", "node_modules"],
+          fileParallelism: false,
+          globalSetup: "specs/integration.setup.ts",
+          include: ["src/**/*.integration.spec.ts"],
+        },
+      },
+    ],
   },
 });
