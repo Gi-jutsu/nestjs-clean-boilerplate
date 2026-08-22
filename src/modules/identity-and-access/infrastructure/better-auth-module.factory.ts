3ddf4d83e17270ff0454a9f741b3b33271493398
Dylan De Sousa <dylan.de-sousa@arsen.co>
2026-08-22 08:12:49 +0200

    refactor: standardize identity drizzle schema names

--- src/modules/identity-and-access/infrastructure/better-auth-module.factory.ts
+++ src/modules/identity-and-access/infrastructure/better-auth-module.factory.ts
@@ -1,12 +1,24 @@
 import { ConfigService } from "@nestjs/config";
 import { IdentityAndAccessEnvironmentKeys } from "@modules/identity-and-access/environment.js";
-import * as schema from "@modules/identity-and-access/infrastructure/database/drizzle.schema.js";
-import { SharedKernelEnvironmentKeys } from "@shared-kernel/environment.js";
+import {
+  accountSchema,
+  sessionSchema,
+  userSchema,
+  verificationSchema,
+} from "@modules/identity-and-access/infrastructure/database/drizzle.schema.js";
+import { SharedKernelEnvironmentKeys } from "@modules/shared-kernel/environment.js";
 import { AuthModule } from "@thallesp/nestjs-better-auth";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+
+const BetterAuthDatabaseSchema = {
+  account: accountSchema,
+  session: sessionSchema,
+  user: userSchema,
+  verification: verificationSchema,
+};
 
 export function createBetterAuthModule() {
   return AuthModule.forRootAsync({
@@ -40,7 +52,7 @@ function createBetterAuth(config: ConfigService) {
     },
     database: drizzleAdapter(client, {
       provider: "pg",
-      schema,
+      schema: BetterAuthDatabaseSchema,
     }),
   });
 }
